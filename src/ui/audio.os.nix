c10fd973c0df1159acd1149fd7e41a37bdfb4536
Knyrps <122410661+Knyrps@users.noreply.github.com>
2026-09-17 04:48:16 +0200

    declare pipewire, enable rtkit

--- src/ui/audio.os.nix
+++ src/ui/audio.os.nix
@@ -1,7 +1,20 @@
 { config, lib, ... }:
 
+let
+  cfg = config.features.audio;
+in
 {
   options.features.audio.enable = lib.mkEnableOption "audio" // {
     default = config.host.has "ui";
+  };
+
+  config = lib.mkIf cfg.enable {
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
   };
 }
