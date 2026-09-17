0dd43290a53e90b455ad3ea3ef11aefa1c3bf0e6
Knyrps <122410661+Knyrps@users.noreply.github.com>
2026-09-17 05:22:41 +0200

    weekly gc, optimise store

--- src/system/nix.os.nix
+++ src/system/nix.os.nix
@@ -2,8 +2,17 @@
 
 {
   nixpkgs.config.allowUnfree = true;
-  nix.settings = {
-    experimental-features = [ "nix-command" "flakes" ];
-    trusted-users = [ "root" "knyrps" ];
+
+  nix = {
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      trusted-users = [ "root" "knyrps" ];
+      auto-optimise-store = true;
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 14d";
+    };
   };
 }
