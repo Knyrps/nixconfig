a0d0e9af57b99a27d50fcf39dae4ced6f0669653
Knyrps <122410661+Knyrps@users.noreply.github.com>
2026-09-17 05:22:42 +0200

    lock on idle and sleep

---
+++ src/ui/idle.home.nix
@@ -0,0 +1,25 @@
+{ osConfig, config, lib, ... }:
+
+let
+  cfg = config.features.idle;
+  has = osConfig.host.has;
+in
+{
+  options.features.idle.enable = lib.mkEnableOption "idle lock" // {
+    default = osConfig.features.niri.enable && has "laptop";
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.swayidle = {
+      enable = true;
+      timeouts = [
+        { timeout = 600; command = "swaylock -f"; }
+        { timeout = 900; command = "niri msg action power-off-monitors"; }
+      ];
+      events = [
+        { event = "before-sleep"; command = "swaylock -f"; }
+        { event = "lock"; command = "swaylock -f"; }
+      ];
+    };
+  };
+}
