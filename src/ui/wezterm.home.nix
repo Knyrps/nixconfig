b225648a2b61edec9ca57b724435dee46142f859
knyrps <knyrps@knyrps.net>
2026-09-21 15:30:42 +0200

    free ctrl+r in wezterm for atuin

--- src/ui/wezterm.home.nix
+++ src/ui/wezterm.home.nix
@@ -21,6 +21,16 @@ in
           use_fancy_tab_bar = false,
           window_close_confirmation = "NeverPrompt",
           window_padding = { left = 8, right = 8, top = 8, bottom = 8 },
+
+          keys = {
+            -- free ctrl+r so it reaches atuin in the shell. wezterm registers
+            -- reload under several normalisations of r/R, so unbind them all
+            -- and move reload to ctrl+F5 (super+r keeps working too).
+            { key = "r", mods = "CTRL",       action = wezterm.action.DisableDefaultAssignment },
+            { key = "R", mods = "CTRL",       action = wezterm.action.DisableDefaultAssignment },
+            { key = "R", mods = "SHIFT|CTRL", action = wezterm.action.DisableDefaultAssignment },
+            { key = "F5", mods = "CTRL",      action = wezterm.action.ReloadConfiguration },
+          },
         }
       '';
     };
