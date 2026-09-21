c23d8fe4075a2510fc157fd071ba7f466f6e4559
knyrps <knyrps@knyrps.net>
2026-09-21 17:01:10 +0200

    remove fish greeting

--- src/tools/cli/fish.os.nix
+++ src/tools/cli/fish.os.nix
@@ -1,16 +1,21 @@
 { config, lib, pkgs, ... }:
 
 let
-  cfg = config.features.fish;
+    cfg = config.features.fish;
 in
 {
   options.features.fish.enable = lib.mkEnableOption "fish" // {
-    default = true;
+      default = true;
   };
 
   config = lib.mkIf cfg.enable {
     # needed at system level so vendor completions land and fish is a valid login shell
-    programs.fish.enable = true;
+    programs.fish = {
+        enable = true;
+        interactiveShellInit = ''
+            set fish_greeting # Disable greeting
+        '';
+    };
     users.users.knyrps.shell = pkgs.fish;
   };
 }
