018681ea560306868857e550a1633505e6fbb1e9
knyrps <knyrps@knyrps.net>
2026-09-22 13:35:04 +0200

    prime offload minecraft

--- src/tools/gaming/prism-launcher.home.nix
+++ src/tools/gaming/prism-launcher.home.nix
@@ -3,6 +3,26 @@
 let
   cfg = config.features.prism-launcher;
   has = osConfig.host.has;
+
+  offload = osConfig.hardware.nvidia.prime.offload.enable or false;
+
+  # Offload Minecraft (LWJGL) to the iGPU via PRIME offload variables.
+  package =
+    if offload then
+      pkgs.symlinkJoin {
+        name = "prismlauncher-offload";
+        paths = [ pkgs.prismlauncher ];
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+        postBuild = ''
+          wrapProgram $out/bin/prismlauncher \
+            --set __NV_PRIME_RENDER_OFFLOAD 1 \
+            --set __NV_PRIME_RENDER_OFFLOAD_PROVIDER NVIDIA-G0 \
+            --set __GLX_VENDOR_LIBRARY_NAME nvidia \
+            --set __VK_LAYER_NV_optimus NVIDIA_only
+        '';
+      }
+    else
+      pkgs.prismlauncher;
 in
 {
   options.features.prism-launcher.enable = lib.mkEnableOption "prism launcher" // {
@@ -10,6 +30,6 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    home.packages = [ pkgs.prismlauncher ];
+    home.packages = [ package ];
   };
 }
